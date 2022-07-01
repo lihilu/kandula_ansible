053f1fe54c3ea45a5f8f7238c7c37c6f52a0c546
lihilu <lihi.reisman@gmail.com>
2022-07-01 18:11:10 +0300

    create db schema and table and user without role

--- ansible/roles/rds/files/create_kandula_db.sql
+++ ansible/roles/rds/files/create_kandula_db.sql
@@ -1,24 +1,27 @@
 -- creating schema: kandula
-DROP SCHEMA IF EXISTS kandula CASCADE;
+DROP SCHEMA IF EXISTS kanduladb CASCADE;
 
-CREATE SCHEMA IF NOT EXISTS kandula;
-AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS kanduladb AUTHORIZATION adminkandula;
  -- creating table instance scheduler with job id, instance id, and shutdown time
-DROP TABLE IF EXISTS kandula.instances_scheduler CASCADE;
+DROP TABLE IF EXISTS kanduladb.instances_scheduler CASCADE;
 
-CREATE TABLE IF NOT EXISTS kandula.instances_scheduler
-	(job_id SERIAL NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS kanduladb.instances_scheduler (
+    job_id SERIAL NOT NULL PRIMARY KEY,
     instance_id character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    shutdown_time integer NOT NULL)
+    shutdown_time integer NOT NULL
+);
 
- ALTER TABLE IF EXISTS kandula.instances_scheduler
- OWNER to postgres;
+
+ ALTER TABLE IF EXISTS kanduladb.instances_scheduler
+ OWNER to adminkandula;
 
 -- create table job log with log id, job id, timestamp and action
-DROP TABLE IF EXISTS kandula.job_log;
+DROP TABLE IF EXISTS kanduladb.job_log;
 
-CREATE TABLE IF NOT EXISTS kandula.job_log
+CREATE TABLE IF NOT EXISTS kanduladb.job_log
 	(log_id SERIAL NOT NULL PRIMARY KEY,
      job_id integer NOT NULL,
      "time" timestamp NOT NULL,
-     action character varying(20) COLLATE pg_catalog."default" NOT NULL)
+     action character varying(20) COLLATE pg_catalog."default" NOT NULL);
+
+CREATE USER kandulaweb WITH PASSWORD 'kandulaweb';
